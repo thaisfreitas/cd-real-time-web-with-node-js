47e3b7ea99bc48ca2bbe52af4fa53c0f0d4c775a
Thais Freitas <thais.freitas@enernoc.com>
2017-06-13 09:41:51 -0300

    add read file in server exercise solution

--- level1.js
+++ level1.js
@@ -4,3 +4,15 @@ var fs = require('fs');
 fs.readFile('index.html', function(error, contents){
   console.log(contents);
 });
+
+// read file in server
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function(request, response) {
+  response.writeHead(200);
+  fs.readFile('index.html', function(error, contents){
+    response.write(contents);
+    response.end();
+  });
+}).listen(8080);
