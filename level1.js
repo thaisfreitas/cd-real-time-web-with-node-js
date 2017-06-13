5145ea1122f0c9b1d31ef0791718434b680e5949
Thais Freitas <thais.freitas@enernoc.com>
2017-06-13 09:48:56 -0300

    add writing response headers solution

--- level1.js
+++ level1.js
@@ -16,3 +16,17 @@ http.createServer(function(request, response) {
     response.end();
   });
 }).listen(8080);
+
+//writing response headers
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function(request, response) {
+  response.writeHead(200,{ 'Content-Type': 'text/html' });
+
+  fs.readFile('index.html', function(err, contents) {
+    response.write(contents);
+    response.end();
+  });
+
+}).listen(8080);
