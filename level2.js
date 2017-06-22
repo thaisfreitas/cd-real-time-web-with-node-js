a4da86be53b06aa929b239ca1a4ca8a8a25a9012
Thais Freitas <thais.freitas@enernoc.com>
2017-06-22 09:24:45 -0500

    add listen close exercise

--- level2.js
+++ level2.js
@@ -49,3 +49,23 @@ server.on('request', function(request, response){
   console.log("New request coming in..." );
 });
 server.listen(8080);
+
+//listen for close
+var http = require('http');
+var server = http.createServer();
+
+server.on('request', function(request, response) {
+  response.writeHead(200);
+  response.write("Hello, this is dog");
+  response.end();
+});
+
+server.on('request', function(request, response) {
+  console.log("New request coming in...");
+});
+server.on('close',function(){
+  console.log("Closing down the server...");
+});
+
+
+server.listen(8080);
