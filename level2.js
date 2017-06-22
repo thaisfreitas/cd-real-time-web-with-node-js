5321af03ed71e247f28ebc9009c01d2b6cbce160
Thais Freitas <thais.freitas@enernoc.com>
2017-06-22 09:22:52 -0500

    add listen twice exercise

--- level2.js
+++ level2.js
@@ -35,3 +35,17 @@ server.on('request', function(request, response){
   response.end();
 });
 server.listen(8080);
+
+//listen twice
+var http = require('http');
+
+var server = http.createServer();
+server.on('request', function(request, response) {
+  response.writeHead(200);
+  response.write("Hello, this is dog");
+  response.end();
+});
+server.on('request', function(request, response){
+  console.log("New request coming in..." );
+});
+server.listen(8080);
