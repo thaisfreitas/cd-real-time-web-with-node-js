607823030b227c63b45e649afef2b2e426c5c470
Thais Freitas <thais.freitas@enernoc.com>
2017-06-22 09:18:50 -0500

    add request event exercise

--- level2.js
+++ level2.js
@@ -24,3 +24,14 @@ chat.on('join', function(nickname) {
 // Emit events here
 chat.emit('join', 'message');
 chat.emit('message', 'message');
+
+//request event
+var http = require('http');
+
+var server = http.createServer();
+server.on('request', function(request, response){
+  response.writeHead(200);
+  response.write("Hello, this is dog");
+  response.end();
+});
+server.listen(8080);
