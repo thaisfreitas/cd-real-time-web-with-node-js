544d5af2387a685e8e986201d401ad49cea7859e
Thais Freitas <thais.freitas@enernoc.com>
2017-06-22 09:09:58 -0500

    add emiting event exercise

--- level2.js
+++ level2.js
@@ -1,7 +1,26 @@
-//chat-emitter 
+//chat-emitter
 var events = require('events');
 var EventEmitter = events.EventEmitter;
 var chat = new EventEmitter();
 chat.on('message', function(message){
   console.log(message);
 });
+
+//emiting events
+var events = require('events');
+var EventEmitter = events.EventEmitter;
+
+var chat = new EventEmitter();
+var users = [], chatlog = [];
+
+chat.on('message', function(message) {
+  chatlog.push(message);
+});
+
+chat.on('join', function(nickname) {
+  users.push(nickname);
+});
+
+// Emit events here
+chat.emit('join', 'message');
+chat.emit('message', 'message');
