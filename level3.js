b9a140fef5903f385194625428051b4b2a13f38e
Thais Freitas <thais.freitas@enernoc.com>
2017-06-26 17:33:44 -0500

    add fixing pipe exercise solution

--- level3.js
+++ level3.js
@@ -1,6 +1,18 @@
-//file piping 
+//file piping
 var fs = require('fs');
 
 var file = fs.createReadStream('fruits.txt');
 
 file.pipe(process.stdout);
+
+//fixing pipe
+
+var fs = require('fs');
+
+var file = fs.createReadStream('origin.txt');
+var destFile = fs.createWriteStream('destination.txt');
+
+file.pipe(destFile, { end: false });
+file.on('end', function(){
+  destFile.end('Finished!');
+});
