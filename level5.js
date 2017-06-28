3cd558c643de9714bde01ca6bb245d2de96c5087
Thais Freitas <thais.freitas@enernoc.com>
2017-06-28 15:10:52 -0500

    add level 5 exercise solutions

---
+++ level5.js
@@ -0,0 +1,9 @@
+//express routes
+var express = require('express');
+var app = express();
+
+app.get('/tweets', function(req, res) {
+  res.sendFile(__dirname + "/tweets.html");
+});
+
+app.listen(8080);
