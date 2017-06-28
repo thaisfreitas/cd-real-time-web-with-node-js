897ff1ba588c20bf7bf241e3a31a737b106e6c5d
Thais Freitas <thais.freitas@enernoc.com>
2017-06-28 15:52:28 -0500

    add params routes exercise solution

--- level5.js
+++ level5.js
@@ -7,3 +7,20 @@ app.get('/tweets', function(req, res) {
 });
 
 app.listen(8080);
+
+//routes params
+var express = require('express');
+var app = express();
+
+var quotes = {
+  'einstein': 'Life is like riding a bicycle. To keep your balance you must keep moving',
+  'berners-lee': 'The Web does not just connect machines, it connects people',
+  'crockford': 'The good thing about reinventing the wheel is that you can get a round one',
+  'hofstadter': 'Which statement seems more true: (1) I have a brain. (2) I am a brain.'
+};
+
+app.get('/quotes/:name', function(req, res){
+  res.end(quotes[req.params.name]);
+});
+
+app.listen(8080);
