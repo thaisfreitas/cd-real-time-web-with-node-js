f7dfd98ef8f870430984ec853d17e4f4766b6d76
Thais Freitas <thais.freitas@enernoc.com>
2017-06-28 16:39:54 -0500

    add wrap up exercise solution

--- level5.js
+++ level5.js
@@ -81,3 +81,24 @@ var resquest = require('request');
 resquest(searchURL, function(error, response, body){
   console.log(body);
 });
+//wrap up
+var url = require('url');
+var request = require('request');
+
+var options = {
+  protocol: "http:",
+  host: "search.twitter.com",
+  pathname: '/search.json',
+  query: {
+    q: "codeschool"
+  }
+};
+
+var searchURL = url.format(options);
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, response){
+  request(searchURL).pipe(response);
+});
+app.listen(8080);
