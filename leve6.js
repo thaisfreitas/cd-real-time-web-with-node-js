4018f285f51b20fac79b1e203ac4613338297007
Thais Freitas <thais.freitas@enernoc.com>
2017-06-30 13:26:54 -0500

    broadcasting questions

--- leve6.js
+++ leve6.js
@@ -27,3 +27,19 @@ server.listen(8080);
   });
 
 </script>
+
+//broadcasting questions
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+io.on('connection', function(client) {
+  console.log("Client connected...");
+
+ client.on('question', function(question) {
+    client.broadcast.emit('question', question);
+  });
+});
+
+server.listen(8080);
