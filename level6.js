f5586508cef201751f8b3f4cb98edcef6f56acd4
Thais Freitas <thais.freitas@enernoc.com>
2017-07-01 10:55:47 -0500

    saving client data

---
+++ level6.js
@@ -0,0 +1,18 @@
+// saving client data
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+io.on('connection', function(client) {
+  console.log("Client connected...");
+
+  client.on('question', function(question) {
+    if(!client.question_asked){
+      client.broadcast.emit('question', question);
+      client.question_asked = true;
+    }
+  });
+});
+
+server.listen(8080);
