022953fce29fd4eadb16b296a0bd7bf95275c0b5
Thais Freitas <tfreitas@thoughtworks.com>
2017-07-27 10:16:41 -0300

    get a key exercise

--- level7.js
+++ level7.js
@@ -3,3 +3,11 @@ var redis = require('redis');
 var client = redis.createClient();
 
 client.set('name', "thais");
+
+//get a key
+var redis = require('redis');
+var client = redis.createClient();
+
+client.get('question', function(error, data){
+  console.log(data);
+});
