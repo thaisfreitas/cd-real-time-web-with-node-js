cfdc1262d3e92d03b11388299f12d72ccb4c919c
Thais Freitas <tfreitas@thoughtworks.com>
2017-07-27 10:19:50 -0300

    add working with lists exercise

--- level7.js
+++ level7.js
@@ -11,3 +11,18 @@ var client = redis.createClient();
 client.get('question', function(error, data){
   console.log(data);
 });
+
+//working with lists
+var redis = require('redis');
+var client = redis.createClient();
+
+var question1 = "Where is the dog?";
+var question2 = "Where is the cat?";
+
+client.lpush("questions",question1 , function(error, value){
+  console.log(value);
+});
+
+client.lpush("questions",question2 , function(error, value){
+  console.log(value);
+});
