21e045a24147bf372c67d2e3f782ab76267ce64c
Thais Freitas <tfreitas@thoughtworks.com>
2017-07-29 16:17:07 -0300

    adding emiting stored questions exercise

--- level7.js
+++ level7.js
@@ -34,3 +34,56 @@ var client = redis.createClient();
 client.lrange("questions", 0, -1, function(error, messagens){
    console.log(messagens);
 });
+
+//persist questions
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var socket = require('socket.io');
+var io = socket.listen(server);
+
+var redis = require('redis');
+var redisClient = redis.createClient();
+
+io.sockets.on('connection', function(client) {
+  client.on('answer', function(question, answer) {
+    client.broadcast.emit('answer', question, answer);
+  });
+
+  client.on('question', function(question) {
+    if(!client.question_asked) {
+      client.question_asked = true;
+      client.broadcast.emit('question', question);
+      // add the question to the list here
+      redisClient.lpush('questions', question);
+    }
+  });
+});
+
+//emiting stored questions
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+var redis = require('redis');
+var redisClient = redis.createClient();
+
+io.sockets.on('connection', function(client) {
+  redisClient.lrange('questions', 0, -1, function(error, questions){
+    questions.forEach(function(question){
+     client.emit('question', question);
+    });
+  });
+  client.on('answer', function(question, answer) {
+    client.broadcast.emit('answer', question, answer);
+  });
+
+  client.on('question', function(question) {
+    if(!client.question_asked) {
+      client.question_asked = true;
+      client.broadcast.emit('question', question);
+      redisClient.lpush("questions", question);
+    }
+  });
+});
